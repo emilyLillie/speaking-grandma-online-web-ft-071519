7f4ecd928d6c6f5424e4ef202d6b0400f50dfd4b
Emily Lillie <emilylillie3@gmail.com>
2019-07-16 23:46:23 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,9 +3,11 @@
 def speak_to_grandma(string)
   if string != string.upcase
     return "HUH?! SPEAK UP, SONNY!"
-  else
-    string.upcase
+  elsif string.upcase
     return "NO, NOT SINCE 1938!"
+  else 
+    string == "I LOVE YOU GRANDMA!"
+    return
   end
 end 
 
